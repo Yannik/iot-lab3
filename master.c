eb7b2c6d60bd9b253223c825e281efa6a3ce6d80
Yannik Sembritzki <yannik@sembritzki.me>
2020-05-24 17:32:22 +0200

    WIP

--- master.c
+++ master.c
@@ -28,6 +28,7 @@
 
 #define TEMP_NOT_RECEIVED -128
 int8_t temperature[NUM_NODES] = {[0 ... NUM_NODES-1] = TEMP_NOT_RECEIVED};
+uint8_t seq_id_received[NUM_NODES] = {[0 ... NUM_NODES-1] = 0};
 
 
 /*---------------------------------------------------------------------------*/
@@ -52,7 +53,13 @@ void input_callback(const void *data, uint16_t len,
 
   if (cmd.command == COMMAND_SEND_TEMP) {
     int src_node_id = linkaddr_to_node_id(src);
-    LOG_INFO("Received temp %d from %u via %u, seq_id %u\n", cmd.data, cmd.sender_id, src_node_id, cmd.seq_id);
+    //LOG_INFO("__Received temp %d from %u via %u, seq_id %u\n", cmd.data, cmd.sender_id, src_node_id, cmd.seq_id);
+    if (cmd.seq_id <= seq_id_received[cmd.sender_id] && seq_id_received[cmd.sender_id] - cmd.seq_id < 128)
+      return;
+    seq_id_received[cmd.sender_id] = cmd.seq_id;
+
+    //int src_node_id = linkaddr_to_node_id(src);
+    LOG_INFO("Received temp %d from %u via %u, seq_id %u after %d hops\n", cmd.data, cmd.sender_id, src_node_id, cmd.seq_id, cmd.hops);
     temperature[cmd.sender_id-1] = cmd.data;
   } else {
     log_unknown_command(cmd, src);
@@ -72,14 +79,18 @@ PROCESS_THREAD(master_process, ev, data)
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
     int max_temp = 0;
+    int received_count = 0;
     int i;
     for (i = 0; i < NUM_NODES; i++) {
       if (temperature[i] > max_temp) {
 	max_temp = temperature[i];
       }
+      if (temperature[i] != TEMP_NOT_RECEIVED) {
+        received_count++;
+      }
       temperature[i] = TEMP_NOT_RECEIVED;
     }
-    LOG_INFO("Max Temp received in the last 120s: %d\n", max_temp);
+    LOG_INFO("Max Temp received in the last 120s: %d Total temps received: %d\n", max_temp, received_count);
 
     etimer_reset(&periodic_timer);
   }
--- slave.c
+++ slave.c
@@ -12,9 +12,11 @@
 #include "dev/leds.h"
 #include "command.h"
 #include "node-id.h"
+#include "heapmem.h"
 
 #include <string.h>
 #include <stdio.h> /* For printf() */
+#include <stdlib.h>
 #include <random.h>
 
 /* Log configuration */
@@ -35,8 +37,11 @@ AUTOSTART_PROCESSES(&slave_process);
 
 /*---------------------------------------------------------------------------*/
 
-void sendData()
-{
+void sendData(void *cmdptr) {
+  struct command cmd =  *(struct command*)cmdptr;
+  LOG_INFO("Sending data: temp %d from %u, seq_id %u\n", cmd.data, cmd.sender_id, cmd.seq_id);
+  nullnet_buf = (uint8_t *)cmdptr;
+  nullnet_len = sizeof(cmd);
   NETSTACK_NETWORK.output(NULL);
 }
 
@@ -44,22 +49,25 @@ void input_callback(const void *data, uint16_t len,
                     const linkaddr_t *src, const linkaddr_t *dest) {
   if (len != sizeof(struct command))
     return;
-
+  //struct command cmd = *(struct command*)heapmem_alloc(sizeof(struct command));
+  struct command* cmd = (struct command*) malloc(sizeof(struct command)):
   static struct command cmd;
-  memcpy(&cmd, data, sizeof(cmd));
+  //memcpy(&cmd, data, sizeof(cmd));
   if (cmd.command == COMMAND_SEND_TEMP) {
+    if (cmd.sender_id == node_id)
+      return;
+    if (cmd.hops > 4)
+      return;
     if (cmd.seq_id <= seq_id_received[cmd.sender_id] && seq_id_received[cmd.sender_id] - cmd.seq_id < 128)
       return;
-
-    int node_id = linkaddr_to_node_id(src);
-    //LOG_INFO("Received temp %d from %u via %u, seq_id %u\n", cmd.data, cmd.sender_id, node_id, cmd.seq_id);
     seq_id_received[cmd.sender_id] = cmd.seq_id;
 
-    nullnet_buf = (uint8_t *)&cmd;
-    nullnet_len = sizeof(cmd);
-    static struct ctimer timer;
-    ctimer_set(&timer, CLOCK_SECOND * (random_rand() % 1000) / 1000, &sendData, NULL);
+    int src_node_id = linkaddr_to_node_id(src);
+    LOG_INFO("Scheduling relaying temp %d from %u via %u, seq_id %u, hops %d\n", cmd.data, cmd.sender_id, src_node_id, cmd.seq_id, cmd.hops);
 
+    cmd.hops++;
+    static struct ctimer timer;
+    ctimer_set(&timer, (CLOCK_SECOND * node_id) / 50, &sendData, (void *)&cmd);
   } else {
     log_unknown_command(cmd, src);
   }
@@ -76,25 +84,25 @@ PROCESS_THREAD(slave_process, ev, data)
   cmd.command = COMMAND_SEND_TEMP;
   cmd.hops = 1;
 
-  nullnet_buf = (uint8_t *)&cmd;
-  nullnet_len = sizeof(cmd);
   nullnet_set_input_callback(input_callback);
 
   fix_randomness(&linkaddr_node_addr);
-  etimer_set(&periodic_timer, CLOCK_SECOND * node_id / 5);
+  etimer_set(&periodic_timer, (120 * CLOCK_SECOND) / (NUM_NODES+1) * (node_id-1));
   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
   etimer_set(&periodic_timer, SEND_INTERVAL);
   while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
     // Generate random temp between -100 and 100
     cmd.data = (random_rand() % 200) - 100;
 
     LOG_INFO("Sending Temperature %d, seq_id %u\n", cmd.data, cmd.seq_id);
+    nullnet_buf = (uint8_t *)&cmd;
+    nullnet_len = sizeof(cmd);
     NETSTACK_NETWORK.output(NULL);
 
     cmd.seq_id++;
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
     etimer_reset(&periodic_timer);
   }  
 
